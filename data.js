b6d99ef3bf596740ae2673bb599cc7840b794653
Chloe Wong <wonghoyee0114@gmail.com>
2023-05-12 11:18:15 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,33 @@
+const characterData = {
+    Buzz: {
+        name: "Buzz",
+        avatar: "images/Buzz.png",
+        health: 80, 
+        diceCount: 3
+    },
+    
+    Pete: {
+        name: "Pete",
+        avatar: "images/Pete.png",
+        health: 20, 
+        diceCount: 1
+    },
+
+    Baby: {
+        name: "Baby",
+        avatar: "images/Baby.png",
+        health: 30,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    Zurg: {
+        name: "Zurg",
+        avatar: "images/Zurg.png",
+        health: 40,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+    
+}
+
+export default characterData
---
+++ index.js
@@ -0,0 +1,78 @@
+import characterData from '/data.js'
+import Character from '/Character.js'
+
+let monstersArray = ["Pete", "Baby", "Zurg"];
+let iswaiting = false
+
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
+
+function attack() {
+    if(!iswaiting) {
+        wizard.getDiceHtml()
+        monster.getDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+
+        if(wizard.dead) {
+            endGame()
+        }
+        else if(monster.dead){
+            iswaiting = true
+            if(monstersArray.length > 0){
+                setTimeout(() => {
+                    monster = getNewMonster()
+                    render()
+                    iswaiting = false
+                }, 1000)
+            }
+            else{
+                endGame()
+            }
+        }
+    }
+    
+}
+
+function endGame() {
+    iswaiting = true 
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Buzz Wins" :
+            "The monster is Victorious"
+
+    const endEmoji = wizard.health > 0 ? " 🚀 " : "☠️"
+
+    setTimeout(() => {
+        document.body.innerHTML = `
+        <div class="end-game">
+            <h2>Game Over</h2>
+            <h3>${endMessage}</h3>
+            <p class="end-emoji">${endEmoji}</p>
+        </div>
+        ` 
+    }, 1200)                
+  
+}
+
+document.getElementById('attack-button').addEventListener("click", attack)
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
+}
+
+const wizard = new Character(characterData.Buzz)
+let monster = getNewMonster()
+render()
+
+
+
+
+
+
+
+
